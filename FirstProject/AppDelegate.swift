2dbd93597d29040288663b4f1aa0dc1786194882
栗原北斗 <martnight1011@gmail.com>
2018-03-23 17:32:18 +0900

    変更AppDelegate

--- FirstProject/AppDelegate.swift
+++ FirstProject/AppDelegate.swift
@@ -12,6 +12,9 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    var name:String!
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
